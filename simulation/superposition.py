MCS Corrected !!! Improved Eddy Viscosity Calculation

simulation/superposition.py
@@ -43,7 +43,20 @@ def interpolate_vec_data(vortex_data_list, t):
     )
 
 def interpolate_local_velocity_field(turbine, X, yloc, zloc, default):
-    """Interpolate vortex field at position X from a list of VortexField objects."""
+    """
+    Interpolate wake velocity field at streamwise position X onto target grid.
+    
+    Args:
+        turbine: Turbine object containing wake_field data
+        X: Streamwise position (global coordinates) where to interpolate
+        yloc: Target lateral grid coordinates (2D array)
+        zloc: Target vertical grid coordinates (2D array)
+        default: Default velocity field for points outside interpolation bounds
+    
+    Returns:
+        Uinterp: Interpolated streamwise velocity field on target grid (yloc, zloc)
+        Uin_interp: Interpolated local input velocity field on target grid (yloc, zloc)
+    """
     vortex_data_list = turbine.wake_field
 
     positions = np.array([v.X for v in vortex_data_list])
@@ -69,7 +82,8 @@ def interpolate_local_velocity_field(turbine, X, yloc, zloc, default):
     U = (1 - alpha) * d0.U + alpha * d1.U
 
     Uinterp = _interp_field(U, source_yloc, source_zloc, yloc, zloc, default=default)
-    return Uinterp
+    Uin_interp = _interp_field(turbine.Uin, source_yloc, source_zloc, yloc, zloc, default=default)
+    return Uinterp, Uin_interp
 
 def interpolate_vortex_field(turbine, X, yloc, zloc, target_pos, default):
     """Interpolate vortex field at position X from a list of VortexField objects."""
@@ -149,20 +163,21 @@ def get_local_velocity_field(config, wind_farm, method='MCS'):
     # Get wake fields at this x-plane
     wake_fields = [interpolate_vortex_field(t, config.pos[0], config.yloc, config.zloc, config.pos, config._init_Uin()) 
                    for t in upstream_turbines]
-
+    
+    local_Uins = np.array([t.Uin for t in upstream_turbines])
     u_yz = np.array([wf.U for wf in wake_fields])
     v_yz = np.array([wf.V for wf in wake_fields])
     w_yz = np.array([wf.W for wf in wake_fields])
 
     # Superpose wakes
-    U, V, W = superpose(U_base, u_yz, v_yz, w_yz, method=method)
+    U, V, W = superpose(U_base, local_Uins, u_yz, v_yz, w_yz, method=method)
 
     return U, V, W
 
-def superpose(U_in, u_yz, v_yz=None, w_yz=None, method='MCS'):
+def superpose(U_in, local_Uins, u_yz, v_yz=None, w_yz=None, method='MCS'):
     """Superpose multiple wake velocity fields using specified method."""
     if method == 'MCS':
-        return momentum_conserving_superposition(U_in, u_yz, v_yz, w_yz)
+        return momentum_conserving_superposition(U_in, local_Uins, u_yz, v_yz, w_yz)
     else:
         return RSS_superposition(U_in, u_yz, v_yz, w_yz)
 
@@ -195,10 +210,11 @@ def RSS_superposition(U_in, u_yz, v_yz=None, w_yz=None):
 
     return U, V, W
 
-def momentum_conserving_superposition(U_in, u_yz, v_yz=None, w_yz=None, max_iter=50, tol=1e-3):
+def momentum_conserving_superposition(U_in, local_Uins, u_yz, v_yz=None, w_yz=None, max_iter=10, tol=1e-3):
     """
     Momentum-Conserving Superposition (MCS) of multiple wake velocity fields.
         U_in: Freestream velocity field (2D array of shape (Ny, Nz))
+        local_Uins: Freestream velocities at each turbine (3D array of shape (i_turbine, Ny, Nz))
         u_yz: Wake velocity fields (3D array of shape (i_turbine, Ny, Nz))
         v_yz: Transverse wake velocity fields (3D array of shape (i_turbine, Ny, Nz)) or None
         w_yz: Vertical wake velocity fields (3D array of shape (i_turbine, Ny, Nz)) or None
@@ -206,7 +222,7 @@ def momentum_conserving_superposition(U_in, u_yz, v_yz=None, w_yz=None, max_iter
     """
 
     # 1. Calculate Individual Deficits (u_i_s)
-    u_s = np.maximum(U_in[None, :, :] - u_yz, 0) # shape (i_turbine, Ny, Nz)
+    u_s = np.maximum(local_Uins - u_yz, 0) # shape (i_turbine, Ny, Nz)
 
     # 2. Calculate Individual Convection Velocities (Uc_i)
     u_c = np.sum(u_yz * u_s, axis=(1,2)) / np.sum(u_s, axis=(1,2)) # shape (i_turbine,)
@@ -218,14 +234,13 @@ def momentum_conserving_superposition(U_in, u_yz, v_yz=None, w_yz=None, max_iter
         U_c = np.maximum(U_c, 1e-6)
 
         weights = u_c / U_c # shape (i_turbine,)
-        # if sum(weights) > len(weights) + 1e-3:
-        # weights_sum = sum(weights)
-        # weights = [w / weights_sum for w in weights]  # normalize weights
+        weights = np.minimum(weights, 1.35)  # cap weights at 1.35 to avoid over-deficit
         U_s = np.sum(weights[:, None, None] * u_s, axis=0)  # shape (Ny, Nz)
         U_s = np.minimum(U_s, U_in)  # prevent over-deficit
 
         U = U_in - U_s
         Uc_new = np.sum(U * U_s, axis=(0,1)) / np.sum(U_s, axis=(0,1))
+        # Uc_new = 0.5 * U_c + (0.5 * Uc_new)
 
         if np.abs(Uc_new - U_c) / np.abs(Uc_new) < tol:
             U_c = Uc_new

simulation/utils.py
@@ -7,52 +7,33 @@ import os
 
 from .superposition import superpose, interpolate_local_velocity_field
 
-def NuT_model(wake_field, config, upstream_turbines):
+def NuT_model(wake_field, config, field_params, upstream_turbines):
     """Compute the turbulent viscosity Nu_T based on the distance from the hub."""
 
-    I_amb = 0.07  # ambient turbulence intensity
-    m = 2.0
+    I_amb = field_params.I_amb  # ambient turbulence intensity
+    m = 2.5
+    turb_dist = wake_field.X
     I = I_amb ** m # initialize as ambient turbulence intensity
     delta_I = lambda turbine, x_val: 0.73 * turbine.a ** 0.8325 * I_amb ** (-0.03) * (x_val / turbine.D) ** -0.32
 
-    if wake_field.X > 5 * config.D:
+    I_norm = np.sqrt(field_params.I_amb ** 2 + delta_I(config, 3 * config.D) ** m)
+
+    if wake_field.X > 3 * config.D:
         I += delta_I(config, wake_field.X) ** m
     else:
-        I += delta_I(config, 5 * config.D) ** m
+        I += delta_I(config, 3 * config.D) ** m
 
     for t in upstream_turbines:
         dist = wake_field.X + (config.pos[0] - t.pos[0])
         I_add = delta_I(t, dist)
         I += I_add ** m
 
-    I_total = I ** (1/m) / 0.1489 * 1.5
+    I_total = I ** (1/m)
 
-    NuT_hat = min(0.03 * (1 - (wake_field.X - 5 * config.D) / (100 * config.D)), wake_field.X / (5 * config.D) * 0.06)
-    NuT_hat = config.a * config._init_Uhub() * config.D * NuT_hat * I_total
+    turb_dist += config.D * I_total / I_norm if len(upstream_turbines) > 0 else 0.0
 
-    for t in upstream_turbines:
-        dist = wake_field.X + (config.pos[0] - t.pos[0])
-        NuT_add = min(0.03 * (1 - (dist - 5 * t.D) / (100 * t.D)), dist / (5 * t.D) * 0.06)
-        NuT_hat += NuT_add * t.a * t._init_Uhub() * t.D * I_total
-
-    # I_amb = 0.07  # ambient turbulence intensity
-    # delta_I = lambda turbine, x_val: 0.73 * turbine.a ** 0.8325 * I_amb ** (-0.03) * (x_val / turbine.D) ** -0.32
-    
-    # nu_t = lambda turbine, I_total: 0.015 * turbine.Uinf * turbine.D * I_total
-    # I = I_amb ** 2 # initialize as ambient turbulence intensity
-
-    # if wake_field.X > 5 * config.D:
-    #     I += delta_I(config, wake_field.X) ** 2
-    # else:
-    #     I += delta_I(config, 5 * config.D) ** 2
-
-    # for t in upstream_turbines:
-    #     dist = wake_field.X + (config.pos[0] - t.pos[0])
-    #     I_add = delta_I(t, dist)
-    #     I += I_add ** 2
-
-    # I_total = np.sqrt(I)
-    # NuT_hat = nu_t(config, I_total)
+    NuT_hat = min(0.03, turb_dist / config.D * 0.006) * config.a * config._init_Uhub() * config.D
+    NuT_hat = NuT_hat * I_total / I_norm
 
     return NuT_hat
 
@@ -125,20 +106,22 @@ def plot_farm_deficit_map(wind_farm, x_resolution=300, y_resolution=100, z_resol
     for i, x_global in enumerate(X_vis):
         
         U_wake_list = []
+        U_in_list = []
         
         for t in wind_farm.turbines:
             dist = x_global - t.pos[0]
             
             if dist > 0 and dist < wind_farm.field_params.max_X * t.D:
                 # Interpolate returns a (Y, Z) slice
-                u_local_abs = interpolate_local_velocity_field(
+                u_local_abs, u_in_local = interpolate_local_velocity_field(
                     t, dist, Y_loc, Z_loc, default=U_in
                 )
                 U_wake_list.append(u_local_abs)
+                U_in_list.append(u_in_local)
 
         if len(U_wake_list) > 0:
             # Pass 2D background and 2D wake list
-            U_total_slice = superpose(U_in, np.array(U_wake_list), method='RSS')[0]
+            U_total_slice = superpose(U_in, np.array(U_in_list), np.array(U_wake_list), method='MCS')[0]
         else:
             U_total_slice = U_in
 
@@ -159,13 +142,13 @@ def plot_farm_deficit_map(wind_farm, x_resolution=300, y_resolution=100, z_resol
     X_grid_xy, Y_grid_xy = np.meshgrid(X_vis, Y_vis) 
     
     im1 = ax1.pcolormesh(X_grid_xy, Y_grid_xy, U_xy_map / Uhub_ref, 
-                       cmap='bwr', shading='auto')
+                       cmap='bwr', shading='auto', vmin=0.3, vmax=1.0)
     
     # XZ Grid (Side view requires X and Z)
     X_grid_xz, Z_grid_xz = np.meshgrid(X_vis, Z_vis)
     
     im2 = ax2.pcolormesh(X_grid_xz, Z_grid_xz, U_xz_map / Uhub_ref, 
-                       cmap='bwr', shading='auto')
+                       cmap='bwr', shading='auto', vmin=0.3, vmax=1.0)
         
     cbar1 = fig.colorbar(im1, ax=ax1)
     cbar1.set_label(r'Normalized Velocity $U / U_{hub}$')